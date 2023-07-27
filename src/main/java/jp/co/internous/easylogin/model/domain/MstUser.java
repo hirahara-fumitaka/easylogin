a9ceb98058a58ad7b239fa5a7dfdc139e3c4c395
hirahara-fumitaka <bz-pleasure98@outlook.jp>
2023-07-27 11:15:41 +0900

    easylogin

---
+++ src/main/java/jp/co/internous/easylogin/model/domain/MstUser.java
@@ -0,0 +1,36 @@
+package jp.co.internous.easylogin.model.domain;
+
+public class MstUser {
+	
+	
+	public int id;
+	public String userName;
+	public String password;
+	public String fullName;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+}
